25c528e76e6a495ec8632fadfca7418d97047c57
Em-Enzo <amine1inzo@gmail.com>
2018-03-12 20:37:54 +0100

    Refactor the code for general use with classes

--- atm solution/atm_refactored.py
+++ atm solution/atm_refactored.py
@@ -5,41 +5,64 @@
 
 
 
-balance = 500 # monny existes 
+class ATM():
+    """docstring for ATM"""
+    def __init__(self, balance, bank_name):
+        self.balance = balance
+        self.bank_name = bank_name 
+    
+    def withdraw(self, request):
+        print ("Welcome to"+self.bank_name)
+        print ("current balance is :: "+str(self.balance))
+        print ("="*30)
+        if(request>self.balance):
+                print ("It seems like you have a fotrune sir, I'am affraid i can't afford that!!")
+                print ("="*30)
+                
+        elif(request<=0):
+                print"Please enter right request"
+        else:
+            self.balance -= request
+            while request>0:
+                if(request>=100 ):
+                    print "give 100"
+                    request=request-100
+                elif(request>=50):
+                    print "give 50"
+                    request=request-50
+                elif(request>=10):
+                    print "give 10"
+                    request=request-10
+                elif(request>=5 ):
+                    print "give 5"
+                    request=request-5
+                else:
+                    print "give "+str(request)
+                    request=0
+        print ("="*30)
+        return self.balance
 
 
-def withdraw(balance, request):
 
-	print ("current balance is :: "+str(balance))
-	if(request>balance):
-    		print ("It seems like you have a fotrune sir, I'am affraid i can't afford that!!")
-    		return -1
-	if(request<=0):
-    		print"Please enter right request"
-	else:
-		balance -= request
-		while request>0:
-    			if(request>=100 ):
-        			print "give 100"
-        			request=request-100
-    			elif(request>=50):
-            			print "give 50"
-            			request=request-50
-    			elif(request>=10):
-                			print "give 10"
-                			request=request-10
-    			elif(request>=5 ):
-            		        	print "give 5"
-                		    	request=request-5
-    			else:
-                	    	 	print "give "+str(request)
-                    		 	request=0
-   		return balance
 
-balance= withdraw(balance, 277)
-balance= withdraw(balance, 30)
-balance= withdraw(balance, 5)
-balance= withdraw(balance, 500)
+
+
+
+
+
+balance1 = 500 # monny existes 
+balance2 = 1000
+
+atm1= ATM(balance1, "Smart Bank")
+atm2= ATM(balance2, "Baraka Bank")
+
+atm1.withdraw(277)
+atm1.withdraw(800)
+
+atm2.withdraw(100)
+atm1.withdraw(2000)
+
+
 
 
 	
