8fbce8adc1ae0fbc0bbce09f5f182aeb16b840ff
Em-Enzo <amine1inzo@gmail.com>
2018-03-16 22:12:50 +0100

    Update the code and add a css file

--- Forums/app.py
+++ Forums/app.py
@@ -1,14 +1,19 @@
 from flask import Flask, render_template
-
+import models
+import stores
 
 
 app = Flask(__name__)
+
+post_store = stores.PostStore()
+post_store.add(models.Post("Life", "Life is alwayes great"))
+post_store.add(models.Post("Sunshine", "Sunshine is amazing"))
 
 
 @app.route("/")
 @app.route("/index/")
 def home():
-	return render_template("index.html")
+	return render_template("index.html", posts = post_store.get_all())
 
 
 @app.route("/SayHello/<name>")
