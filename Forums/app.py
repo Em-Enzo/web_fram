df29c80d255d7fe27f0b422558e314845e558104
Em-Enzo <amine1inzo@gmail.com>
2018-03-17 23:17:06 +0100

    Ogranize code and improve the insert methods

--- Forums/app.py
+++ Forums/app.py
@@ -1,25 +1,14 @@
 from flask import Flask, render_template
 import models
 import stores
+import dummy_data
+from views import *
 
-
-app = Flask(__name__)
-
-post_store = stores.PostStore()
-post_store.add(models.Post("Life", "Life is alwayes great"))
-post_store.add(models.Post("Sunshine", "Sunshine is amazing"))
-
-
-@app.route("/")
-@app.route("/index/")
-def home():
-	return render_template("index.html", posts = post_store.get_all())
-
-
-@app.route("/SayHello/<name>")
-def say_hello(name):
-	return "Hello %s " % name           #.format(name)
 		
+member_store = stores.MemberStore()
+post_store = stores.PostStore()
 
 
-app.run()
+if __name__ == "__main__":
+	dummy_data.seed_stores(member_store, post_store)
+	app.run()
